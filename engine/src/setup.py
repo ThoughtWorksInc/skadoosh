4c4c9107f04d48c96e066c5af92967d38f778e01
cackharot <cackharot@gmail.com>
2016-06-12 10:12:08 +0530

    add deploy file

--- engine/src/setup.py
+++ engine/src/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='skadoosh.core',
+    name='skadoosh',
     version='0.1',
-    packages=find_packages(),
+    packages=['agent_portal','engine/src/api','engine/src/core'],
     url='www.skadoosh.in',
     license='Apache License 2.0',
     author='cackharot',
     author_email='cackharot@gmail.com',
     long_description='True virtual agent help',
     zip_safe=False,
-    install_requires=['Flask', 'nltk']
+    install_requires=['Flask', 'nltk'],
+    data_files = [('engine/src/api',['engine/src/api/application.cfg']),
+                  ('engine/src', ['engine/src/requirements.txt'])]
 )
